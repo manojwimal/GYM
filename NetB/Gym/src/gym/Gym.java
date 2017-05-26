950e67177a304298640cf1fe6c060ff4dfe3ac2d
Manoj Wimal <manojwimal@gmail.com>
2017-05-26 21:39:15 +0530

    inital commit

---
+++ NetB/Gym/src/gym/Gym.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gym;
+
+/**
+ *
+ * @author ManoJ
+ */
+public class Gym {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here        
+        Login jf= new Login();
+        jf.setVisible(true);
+        
+    }
+}
